3da90edb9e1c83e352365e06ac23b90735eba51f
xela1601 <schrein2@hm.edu>
2018-11-20 14:31:11 +0100

    Modell weiterentwickelt

--- src/main/java/guideLines/model/Address.java
+++ src/main/java/guideLines/model/Address.java
@@ -1,5 +1,53 @@
 package main.java.guideLines.model;
 
 public class Address {
+    private String name;
+    private final String street;
+    private final int streetNumber;
+    private final String city;
+    private final int postCode;
+    private Station nearestStation;
+    private FormOfTransport preferedWayOfTransport;
+    
+    Address(String name, String street, int streetNumber, String city, int postCode){
+        this.name = name;
+        this.street = street;
+        this.streetNumber = streetNumber;
+        this.city = city;
+        this.postCode = postCode;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setFormOfTransport(FormOfTransport preferedWayOfTransport){
+        this.preferedWayOfTransport = preferedWayOfTransport;
+    }
+    public FormOfTransport getFormOfTransport(){
+        return this.preferedWayOfTransport;
+    }
+    
+    public void setNearestStation(Station nearestStation){
+        this.nearestStation = nearestStation;
+    }
+    
+    public String getFullAddress(){
+        return street + " " + streetNumber + "\n" + postCode + " " + city;
+    }
+    public String getStreet(){
+        return this.street;
+    }
+    public int getStreetNumber(){
+        return streetNumber;
+    }
+    public String getCity(){
+        return this.city;
+    }
+    public int getPostCode(){
+       return this.postCode;
+    }
+    
 
 }
--- src/main/java/guideLines/model/Profile.java
+++ src/main/java/guideLines/model/Profile.java
@@ -6,9 +6,51 @@ public class Profile {
 	private Address homeAddress;
 	private Address[] destinationAddresses = new Address[3];
 	
-	Profile() {
-		
-		
+	Profile(String firstName, String userNameID) {
+		this.firstName = firstName;
+		this.userNameID = userNameID;
 	}
+        
+        public void setFirstName(String firstName){
+            this.firstName = firstName;
+        }
+        public String getFirstName(){
+            return this.firstName;
+        }
+        public void setUserNameID(String userNameID){
+            this.userNameID = userNameID;
+        }
+        public String getUserNameID(){
+            return this.userNameID;
+        }
+        public void setHomeAddress(Address homeAddress){
+            this.homeAddress = homeAddress;
+        }
+        public Address getHomeAddress(){
+            return this.homeAddress;
+        }
+        public void addDestinationAddress(Address newDestination) throws fullDestinationsException{
+            if(destinationsFull())
+                throw new fullDestinationsException();
+            else {
+                for(int i = 0; i<3 ; i++){
+                    if(destinationAddresses[i] == null){
+                        destinationAddresses[i] = newDestination;
+                        break;
+                    }
+                }
+                               
+            }
+            
+        }
+        public void removeDestinationAddress(Address toRemove){
+            for(int i = 0; i<3 ; i++)
+                destinationAddresses[i] = destinationAddresses[i].equals(toRemove)? null : destinationAddresses[i];
+        }
+        
+        private boolean destinationsFull(){
+           return destinationAddresses[0] != null && destinationAddresses[1] != null && destinationAddresses[2] != null;
+        }
+        
 
 }
