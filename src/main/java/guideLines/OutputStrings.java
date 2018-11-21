dee94a4af2a94d53d40caea14786c2cb8b09bd95
Bratulescu Beniamin <bratules@hm.edu>
2018-11-21 23:20:53 +0100

    Einrichtung und Antwort geändert

--- src/main/java/guideLines/OutputStrings.java
+++ src/main/java/guideLines/OutputStrings.java
@@ -7,8 +7,9 @@ package main.java.guideLines;
  */
 public class OutputStrings {
 
-	public static final String WELCOME_EINRICHTUNG = "Willkommen beim GuideLines Einrichtungsassistenten!<break time=\"0.1s\" /> "
-			+ "Die Einrichtung dauert circa fünf Minuten. <break time=\"0.05s\" /> Möchtest du die Einrichtung jetzt starten?";
+//	public static final String WELCOME_EINRICHTUNG = "Willkommen beim GuideLines Einrichtungsassistenten!<break time=\"0.1s\" /> "
+//			+ "Die Einrichtung dauert circa fünf Minuten. <break time=\"0.05s\" /> Möchtest du die Einrichtung jetzt starten?";
+	public static final String WELCOME_EINRICHTUNG = "Wo würdest du gerne hinfahren?";
 	public static final String WELCOME_EINRICHTUNG_REPROMPT = "Du befindest dich im Hauptmenü des GuideLines Einrichtungsassistenten. "
 			+ "<break time=\"0.05s\" /> Möchtest du die Einrichtung jetzt starten?";
 
--- src/main/java/guideLines/handlers/BusStatusIntentHandler.java
+++ src/main/java/guideLines/handlers/BusStatusIntentHandler.java
@@ -1,12 +1,18 @@
 package main.java.guideLines.handlers;
 
 import static com.amazon.ask.request.Predicates.intentName;
+import static main.java.guideLines.handlers.WhatsMyColorIntentHandler.COLOR_SLOT;
 
+import java.util.Map;
 import java.util.Optional;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Intent;
+import com.amazon.ask.model.IntentRequest;
+import com.amazon.ask.model.Request;
 import com.amazon.ask.model.Response;
+import com.amazon.ask.model.Slot;
 
 public class BusStatusIntentHandler implements RequestHandler {
 
@@ -21,8 +27,16 @@ public class BusStatusIntentHandler implements RequestHandler {
 	@Override
 	public Optional<Response> handle(HandlerInput input) {
 		
+		Request request = input.getRequestEnvelope().getRequest();
+        IntentRequest intentRequest = (IntentRequest) request;
+        Intent intent = intentRequest.getIntent();
+        Map<String, Slot> slots = intent.getSlots();
+
+        // Get the color slot from the list of slots.
+        Slot destinationSlot = slots.get(DESTINATION_SLOT);
+		
 		return input.getResponseBuilder()
-				.withSpeech("Ich kann dir momentan nichts sagen")
+				.withSpeech("Willst du wirklich nach " + destinationSlot.getValue() + " fahren?")
 				.withSimpleCard("Bus direction", "Ich kann dir momentan nichts sagen").build();
 	}
 
