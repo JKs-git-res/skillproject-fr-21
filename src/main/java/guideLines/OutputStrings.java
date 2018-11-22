e9d77e60b62fb904d3355bb04eb5180c2f4126e3
Schreiner <a.schreiner0@hm.edu>
2018-11-22 13:18:22 +0100

    .travis.yml updated

--- src/main/java/guideLines/OutputStrings.java
+++ src/main/java/guideLines/OutputStrings.java
@@ -1,6 +1,6 @@
 package main.java.guideLines;
 /** In dieser Klasse befinden sich alle Strings, die von Alexa ausgegeben werden.
- *  Somit ist der Code übersichtlicher.
+ *  Somit ist der Code uebersichtlicher.
  * 
  * @author hm-schrein2
  *
@@ -8,7 +8,7 @@ package main.java.guideLines;
 public class OutputStrings {
 
 //	public static final String WELCOME_EINRICHTUNG = "Willkommen beim GuideLines Einrichtungsassistenten!<break time=\"0.1s\" /> "
-//			+ "Die Einrichtung dauert circa fünf Minuten. <break time=\"0.05s\" /> Möchtest du die Einrichtung jetzt starten?";
+//			+ "Die Einrichtung dauert circa fünf Minuten. <break time=\"0.05s\" /> Moechtest du die Einrichtung jetzt starten?";
 	public static final String WELCOME_EINRICHTUNG = "Wo würdest du gerne hinfahren?";
 	public static final String WELCOME_EINRICHTUNG_REPROMPT = "Du befindest dich im Hauptmenü des GuideLines Einrichtungsassistenten. "
 			+ "<break time=\"0.05s\" /> Möchtest du die Einrichtung jetzt starten?";
