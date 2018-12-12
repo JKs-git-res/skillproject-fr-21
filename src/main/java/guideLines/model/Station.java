eec941e43a29a63fb1e4ca9373cdaf85c31ccf45
DCLOC\aleksandar.ivanov <aleksandar.ivanov@smartify-it.de>
2018-12-12 13:20:30 +0100

    fix merge error caused by alex

--- src/main/java/guideLines/model/Station.java
+++ src/main/java/guideLines/model/Station.java
@@ -1,20 +1,43 @@
 package main.java.guideLines.model;
 
+import java.util.HashMap;
+
 public class Station {
-    private final String name;
-    private final FormOfTransport typeOfStation;
-    
-    public Station(String name, FormOfTransport typeOfStation) {
-        this.name = name;
-        this.typeOfStation = typeOfStation;
-    }
-    
-    public String getName(){
-        return this.name;
-    }
-    
-    public FormOfTransport getTypeOfStation(){
-        return this.typeOfStation;
-    }
-    
+
+  private final String name;
+  private final HashMap<String, FormOfTransport> lines;
+  private final String id;
+  private final String city;
+
+  /**
+   * Station object. It represents a station from Here API.
+   *
+   * @param name Name of the station
+   * @param id Station id from Here API
+   * @param lines A HashMap with all the public transportation names and the
+   * form of transport defined in {@link FormOfTransport}
+   */
+  public Station(String name, String id, String city, HashMap<String, FormOfTransport> lines) {
+    this.name = name;
+    this.lines = lines;
+    this.id = id;
+    this.city = city;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public HashMap<String, FormOfTransport> getLines() {
+    return this.lines;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
 }
