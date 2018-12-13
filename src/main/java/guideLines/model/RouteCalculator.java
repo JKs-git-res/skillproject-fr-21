b2b3774025229896a5ef602abbac534190a48d86
Bratulescu Beniamin <bratules@hm.edu>
2018-12-13 23:01:35 +0100

    route und plan separat

--- src/main/java/guideLines/model/RouteCalculator.java
+++ src/main/java/guideLines/model/RouteCalculator.java
@@ -4,37 +4,41 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RouteCalculator {
 	
+	/**
+	 * Tells the exact route where and where to change and how many stops to ride the {@link FormOfTransport}.
+	 * @param departure
+	 * 					The station where to start from
+	 * @param arrival
+	 * 					The destination station
+	 * @return
+	 * 					The complete route with the time and stops number.
+	 * @throws IOException
+	 */
 	public String getRoute(Station departure, Station arrival) throws IOException {
-		String response = new AddressResolver().getResponseFromURL("https://transit.api.here.com/v3/route.json"
-				+ "?app_id=" + AddressResolver.app_id
-				+ "&app_code=" + AddressResolver.app_code
-				+ "&routing=all"
-				+ "&dep=" + departure.getLatitude() + "," + departure.getLongitude()
-				+ "&arr=" + arrival.getLatitude() + "," + arrival.getLongitude()
-				+ "&time=" + getTimeFormatted());
-
 		StringBuilder plan = new StringBuilder();
-		
-		JSONArray connections = new JSONObject(response).getJSONObject("Res").getJSONObject("Connections").getJSONArray("Connection");
-		JSONObject lastChoice = connections.getJSONObject(connections.length()-1);
-		JSONArray sections = lastChoice.getJSONObject("Sections").getJSONArray("Sec");
+		JSONArray connections = new JSONObject(getJSONresponse(departure, arrival))
+				.getJSONObject("Res").getJSONObject("Connections").getJSONArray("Connection");
+		JSONObject choice = getNextConnection(connections);
+		JSONArray sections = choice.getJSONObject("Sections").getJSONArray("Sec");
 		for (int i=0; i<sections.length(); i++) {
 			JSONObject section = sections.getJSONObject(i);
+			String time = section.getJSONObject("Dep").getString("time").split("T")[1];
+			time = getTimePretty(time);
 			int mode = section.getInt("mode");
 			if (mode != 4 && mode != 5 && mode != 7 && mode != 8) {
 				continue;
 			}
-			String time = section.getJSONObject("Dep").getString("time").split("T")[1];
-			time = time.substring(0, time.length()-3);
 			String direction = section.getJSONObject("Dep").getJSONObject("Transport").getString("dir");
 			String name = section.getJSONObject("Dep").getJSONObject("Transport").getString("name");
 			String category = section.getJSONObject("Dep").getJSONObject("Transport").getJSONObject("At").getString("category");
@@ -46,9 +50,153 @@ public class RouteCalculator {
 			} else {
 				plan.append(" in die ");
 			}
-			plan.append(category).append(" ").append(name).append(" einsteigen und ").append(stops).append(" Stationen in die Richtung ").append(direction).append(" bis ").append(arr).append(" fahren. ");
+			plan.append(category).append(" ").append(name).append(" einsteigen und ");
+			if (stops == 1) {
+				plan.append("eine Station in die Richtung ");
+			} else {
+				plan.append(stops).append(" Stationen in die Richtung ");
+			}
+			plan.append(direction).append(" bis ").append(arr).append(" fahren. ");
 		}
 		return plan.toString();
+	}
+	
+	/**
+	 * Tells the plan how to get from start to destination station.
+	 * But doesn't tell when is the next one. It tells just how to get there and the time of the trip.
+	 * @param departure
+	 * 					The station where to start
+	 * @param arrival
+	 * 					The destination station
+	 * @return
+	 * 					The plan how to get from A to B
+	 * @throws JSONException
+	 * 					If the JSON response from Here API was messed up
+	 * @throws IOException
+	 * 					If there was a problem with the connection
+	 */
+	public String getPlan(Station departure, Station arrival) throws JSONException, IOException {
+		StringBuilder plan = new StringBuilder();
+		JSONArray connections = new JSONObject(getJSONresponse(departure, arrival))
+				.getJSONObject("Res").getJSONObject("Connections").getJSONArray("Connection");
+		JSONObject choice = getNextConnection(connections);
+		JSONArray sections = choice.getJSONObject("Sections").getJSONArray("Sec");
+		String duration = choice.getString("duration").replaceAll("PT", "");
+		String hours = duration.split("H")[0];
+		String minutes = duration.split("H")[1].replaceAll("M", "");
+		int transfers = choice.getInt("transfers");
+		boolean started = false;
+		for (int i=0; i<sections.length(); i++) {
+			JSONObject section = sections.getJSONObject(i);
+			int mode = section.getInt("mode");
+			if (mode != 4 && mode != 5 && mode != 7 && mode != 8) {
+				continue;
+			}
+			String name = section.getJSONObject("Dep").getJSONObject("Transport").getString("name");
+			String category = section.getJSONObject("Dep").getJSONObject("Transport").getJSONObject("At").getString("category");
+			String arr = section.getJSONObject("Arr").getJSONObject("Stn").getString("name");
+			if (mode != 5 && !started) {
+				plan.append("Nimm die ");
+				started = true;
+			} else if (mode == 5 && !started) {
+				plan.append("Nimm den ");
+				started = true;
+			} else if (mode != 5 && started) {
+				plan.append(" die ");
+			} else {
+				plan.append(" den ");
+			}
+			plan.append(category).append(" ").append(name).append(" nach ").append(arr);
+			if (transfers != 0) {
+				plan.append(" und von dort");
+				transfers--;
+			}
+		}
+		plan.append(". Die Reise dauert ");
+		if (!hours.equals("0")) {
+			plan.append(hours).append(" Stunden");
+		}
+		if (!hours.equals("0") && !minutes.equals("0")) {
+			if (minutes.startsWith("0")) {
+				minutes = minutes.replaceAll("0", "");
+			}
+			plan.append(hours).append(" Stunden und ").append(minutes).append(" Minuten");
+		} else if (!hours.equals("0") && !minutes.equals("0")) {
+			plan.append(hours).append(" Stunden");
+		} else if (hours.equals("0")) {
+			plan.append(minutes).append(" Minuten");
+		}
+		return plan.toString();
+	}
+	
+	/**
+	 * Returns time in german for Alexa to say
+	 * @param time
+	 * 				Time in the format HH:mm
+	 * @return
+	 * 				Time in german example: 10:10 -> 10 Uhr 10
+	 */
+	private String getTimePretty(String time) {
+		time = time.substring(0, time.length()-3);
+		String hour = time.split(":")[0];
+		String minute = time.split(":")[1];
+		if (hour.startsWith("0")) {
+			hour = hour.replaceAll("0", "");
+		}
+		if (minute.startsWith("0")) {
+			minute = minute.replaceAll("0", "");
+		}
+		return hour + " Uhr " + minute;
+	}
+	
+	/**
+	 * Some connection are in the past (the bus already left).
+	 * This method gives back the next available connection.
+	 * @param connections
+	 * 					A {@link JSONArray} from the Here API response with all the connections
+	 * @return
+	 * 					The next available connection
+	 */
+	private JSONObject getNextConnection(JSONArray connections) {
+		SimpleDateFormat parser = new SimpleDateFormat("HH:mm");
+		JSONObject choice = connections.getJSONObject(0);
+		for (int j=0; j<connections.length(); j++) {
+			choice = connections.getJSONObject(j);
+			String dep = choice.getJSONObject("Dep").getString("time").split("T")[1];
+			dep = dep.substring(0, dep.length()-3);
+			Date now = null;
+			try {
+				now = parser.parse(parser.format(new Date()));
+				if (now.before(parser.parse(dep))) {
+					return choice;
+				}
+			} catch (ParseException e) {
+				throw new RuntimeException();
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Gets the JSON response from the Here API
+	 * @param departure
+	 * 					The station where to start from
+	 * @param arrival
+	 * 					The destination station
+	 * @return
+	 * 					The JSON response from Here API
+	 * @throws IOException
+	 * 					If there was a problem with the connection
+	 */
+	private String getJSONresponse(Station departure, Station arrival) throws IOException {
+		String response = new AddressResolver().getResponseFromURL("https://transit.api.here.com/v3/route.json"
+				+ "?app_id=" + AddressResolver.app_id
+				+ "&app_code=" + AddressResolver.app_code
+				+ "&routing=all"
+				+ "&dep=" + departure.getLatitude() + "," + departure.getLongitude()
+				+ "&arr=" + arrival.getLatitude() + "," + arrival.getLongitude()
+				+ "&time=" + getTimeFormatted());
+		return response;
 	}
 	
 	/**
--- src/test/java/guideLines/model/RouteCalculatorTest.java
+++ src/test/java/guideLines/model/RouteCalculatorTest.java
@@ -5,22 +5,35 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 
 import guidelines.model.FormOfTransport;
 import guidelines.model.RouteCalculator;
 import guidelines.model.Station;
-
+/**
+ * Da die Outputs sich st‰ndig ‰ndern teste ich nur ob der String leer ist oder nicht.
+ * @author Benny
+ *
+ */
 class RouteCalculatorTest {
 	
 	private RouteCalculator rc = new RouteCalculator();
 
 	@Test
 	void CalculateRouteTest() throws IOException {
-		Station departure = new Station("","","", new HashMap<String, FormOfTransport>(), 48.15427, 11.55383); // Lothstra√üe
+		Station departure = new Station("","","", new HashMap<String, FormOfTransport>(), 48.15427, 11.55383); // Lothstraﬂe
 		Station arrival = new Station("","","", new HashMap<String, FormOfTransport>(), 48.17871, 11.55667); // Olympiapark
 		String route = rc.getRoute(departure, arrival);
 		assertEquals(false,route.isEmpty());
+	}
+	
+	@Test
+	void GetPlanTest() throws JSONException, IOException {
+		Station departure = new Station("","","", new HashMap<String, FormOfTransport>(), 48.15427, 11.55383); // Lothstraﬂe
+		Station arrival = new Station("","","", new HashMap<String, FormOfTransport>(), 48.17871, 11.55667); // Olympiapark
+		String route = rc.getPlan(departure, arrival);
+		assertEquals(false, route.isEmpty());
 	}
 
 }
